f4184bce21bf815498311a4ee0a04d14f119dd5c
1-apex <pratham153patil@gmail.com>
2025-04-12 05:29:45 -0500

    admins update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,21 @@ const adminEmails = [
   "pratham153patil@gmail.com",
   "thesamarthjagtap@gmail.com",
   "linkwithsujit@gmail.com",
-  "satya.rallabandi7@gmail.com"
+  "satya.rallabandi7@gmail.com",
+  "rr1shahzad@gmail.com",
+  "zecilyjain2000@gmail.com",
+  "abhijitchallaplli99@gmail.com",
+  "clivin100@gmail.com",
+  "desair2003@gmail.com",
+  "akhilcherukuri54@gmail.com",
+  "lamia.rodoshi95@gmail.com",
+  "saitejar35@gmail.com",
+  "araohatk@gmail.com",
+  "pradeepsinhchavda1672@gmail.com",
+  "aastha6100@gmail.com",
+  "pranachilukuri22@gmail.com",
+  "kanishkaabharti@gmail.com",
+  "ramialet02@gmail.com"
 ];
 
 export default function DashboardLayout({
